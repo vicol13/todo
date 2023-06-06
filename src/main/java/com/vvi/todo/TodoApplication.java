c839d90c7bbd700ce5e30e11fb0f641d5b777681
Valeriu Vicol <valeriu.vicol@mirriad.com>
2023-06-06 11:32:09 +0200

    set up app skeleton

---
+++ src/main/java/com/vvi/todo/TodoApplication.java
@@ -0,0 +1,14 @@
+package com.vvi.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
